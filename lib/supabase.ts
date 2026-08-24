a7d86c1a3a2e4640f2cba49dcfd4d59dcf011346
Kevin Machado <buzzentt@gmail.com>
2026-08-24 16:42:10 -0300

    Use Supabase anon key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -8,11 +8,11 @@ export function createClient() {
   }
 
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   if (!supabaseUrl || !supabaseKey) {
     throw new Error(
-      'Faltan NEXT_PUBLIC_SUPABASE_URL o NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY'
+      'Faltan NEXT_PUBLIC_SUPABASE_URL o NEXT_PUBLIC_SUPABASE_ANON_KEY'
     );
   }
 
